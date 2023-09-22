4bcfa499b9b167b2cc5a70f0b54afaf17f82f1d0
Eduardo Martins <eduxd60.20@hotmail.com>
2023-09-22 09:13:54 -0300

    update 2

--- acesso/acesso.js
+++ acesso/acesso.js
@@ -1,83 +1,57 @@
-// const btn = document.querySelector("#btn")
+ const btn = document.querySelector("#btn")
 
-// const usuario = document.querySelector(".usuario").value;
+ const usuario = document.querySelector(".usuario").value;
 
-// const senha = document.querySelector(".senha").value;
-
-
-// function send(){
-
-// const usuario = document.querySelector(".usuario").value;
-
-// const senha = document.querySelector(".senha").value;
-
-// const preencha = document.querySelector("#preencha")
-
-// const sucesso = document.querySelector("#sucesso")
+ const senha = document.querySelector(".senha").value;
 
 
-// if(usuario.length <1 || senha.length <1){
+ function send(){
+
+ const usuario = document.querySelector(".usuario").value;
+
+ const senha = document.querySelector(".senha").value;
+
+ const preencha = document.querySelector("#preencha")
+
+ const sucesso = document.querySelector("#sucesso")
+
+
+ if(usuario.length <1 || senha.length <1){
     
-//     preencha.style.display = ('inline')
+     preencha.style.display = ('inline')
 
-//     setTimeout(() => {
+     setTimeout(() => {
 
-//         preencha.style.display = ('none')
+         preencha.style.display = ('none')
     
-//         }, 1000);
+         }, 1000);
 
-//     return false;
-// }
+     return false;
+ }
 
-// if(usuario == "bitrix" && senha == "movida"){
+ if(usuario == "bitrix" && senha == "movida"){
 
-//     sucesso.style.display = ('inline')
+     sucesso.style.display = ('inline')
 
-//     setTimeout(() => {
+     setTimeout(() => {
 
-//     location.href = "http://127.0.0.1:5500/home/home.html";
+     location.href = "https://edu221g.github.io/bitrix/home/home";
 
-//     }, 500);
+     }, 500);
     
-// } else{
+ } else{
 
-//     negado.style.display = ('inline')
+     negado.style.display = ('inline')
 
-//     setTimeout(() => {
+     setTimeout(() => {
 
-//         location.href = "http://127.0.0.1:5500/error/erro.html";
+         location.href = "https://edu221g.github.io/bitrix/erro/erro";
     
-//         }, 500);
-
-// };
-
-
-// };
-
-function send() {
-    const usuario = document.querySelector(".usuario").value;
-    const senha = document.querySelector(".senha").value;
-    const preencha = document.querySelector("#preencha");
-    const sucesso = document.querySelector("#sucesso");
-    const negado = document.querySelector("#negado"); // Adicionei esta linha
-
-    if (usuario.length < 1 || senha.length < 1) {
-        preencha.style.display = 'inline';
-        setTimeout(() => {
-            preencha.style.display = 'none';
-        }, 1000);
-        return false;
-    }
-
-    if (usuario === "bitrix" && senha === "movida") {
-        sucesso.style.display = 'inline';
-        setTimeout(() => {
-            location.href = "https://edu221g.github.io/bitrix/home/home";
         }, 500);
-    } else {
-        negado.style.display = 'inline';
-        setTimeout(() => {
-            location.href = "https://edu221g.github.io/bitrix/erro/erro";
-        }, 500);
-    }
-}
+
+ };
+
+
+ };
+
+
